745d0ba890ef94335285b2e6b0a508b377e91938
Jay <jay1904prajapati@gmail.com>
2025-09-07 22:05:08 +0530

    add gitignore and cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
 import emailRoutes from "./routes/emailRoutes.js";
 
 const app = express();
+app.use(
+  cors({
+    origin: [
+      "https://getting-location-osw3.vercel.app",
+      "http://localhost:5173",
+    ], // Allow the frontend's origin
+    credentials: true, // Allow cookies/credentials
+  })
+);
 app.use(express.json());
 
 // Routes
